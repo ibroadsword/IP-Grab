6453c0d899defb6cf7479cc135f3722e8e20c581
ibroadsword <32231254+ibroadsword@users.noreply.github.com>
2017-12-03 16:51:01 -0600

    Update get_ip.py

--- get_ip.py
+++ get_ip.py
@@ -1,37 +1,43 @@
-import socket
 import tkinter as tk
+import tkinter.ttk as ttk
+import socket
 
-class GUI(tk.Frame):
+class GUI(ttk.Frame):
 
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.pack()
 		self.create_widgets()
 
-	
+
 	def create_widgets(self):
-		self.label = tk.Label(self, text="Enter Hostname")
+		self.label = ttk.Label(self, text="Enter Hostname")
 		self.label.pack()
 
-		self.entry = tk.Entry(self, bd=4)
+		self.entry = ttk.Entry(self)
 		self.entry.pack()
 
-		self.ip_button = tk.Button(self, text="Get IP", command=lambda : self.get_ip(self.entry.get()))
+		self.ip_button = ttk.Button(self, text="Get IP", command=lambda : self.get_ip(self.entry.get()))
 		self.ip_button.pack(side="bottom")
 
 
 	def get_ip(self, hostname):
 		try:
 			ip = socket.gethostbyname(hostname)
-			self.entry.delete(0, len(self.entry.get()))
-			self.entry.insert(0, ip)
+			self.change_entry(self.entry.get(), ip)
 		except:
-			self.entry.delete(0, len(self.entry.get()))
-			self.entry.insert(0, "Invalid Hostname")
+			self.change_entry(self.entry.get(), "Invalid Hostname")
+
+	def change_entry(self, original_entry, new_entry):
+		self.entry.delete(0, len(original_entry))
+		self.entry.insert(0, new_entry)
 
 
 def main():
 	root = tk.Tk()
+	root.geometry("300x125")
+	root.title("Hostname Resolver")
+
 	gui = GUI(root)
 	gui.mainloop()
 
